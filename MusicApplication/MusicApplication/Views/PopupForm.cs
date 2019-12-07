11100a9abaaff98186bc480611eebe621f0803ab
Samuel Wynn <wynns@spu.edu>
2019-12-07 15:46:55 -0800

    Popups!

---
+++ MusicApplication/MusicApplication/Views/PopupForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicApplication.Views
+{
+    public partial class PopupForm : Form
+    {
+        public PopupForm(string diaglog)
+        {
+            _popupFormDialog.Text = diaglog;
+            InitializeComponent();
+        }
+
+        private void popupButtonClose(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
